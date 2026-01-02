dab7e7dc49679580b6ea8562928b601da527448b
Dawid Łakomski <dawid.lakomski@gowork.pl>
2026-01-02 13:54:02 +0100

    Lower monorepo builder version

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $config): void {
     $config->packageDirectories([__DIR__.'/packages']);
@@ -13,7 +11,7 @@ return static function (MBConfig $config): void {
     $config->dataToAppend([
         ComposerJsonSection::REQUIRE_DEV => [
             'phpunit/phpunit' => '^11.5',
-            'symplify/monorepo-builder' => '^12.4',
+            'symplify/monorepo-builder' => '^11.2',
         ],
     ]);
 };
