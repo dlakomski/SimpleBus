2bcdeae748c20ead649c4de34413981410f33bdd
Dawid Łakomski <dawid.lakomski@gowork.pl>
2026-01-03 15:06:42 +0100

    Fix listing packages

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $config): void {
     $config->packageDirectories([__DIR__.'/packages']);
@@ -13,5 +17,12 @@ return static function (MBConfig $config): void {
             'phpunit/phpunit' => '^11.5.46',
             'symplify/monorepo-builder' => '^11.2',
         ],
+    ]);
+
+    $config->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
     ]);
 };
